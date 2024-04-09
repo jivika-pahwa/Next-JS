5de8cdc64f77a14d6e2d71aaf970d6c57b7e3e1b
Jivika Pahwa <jivika.pahwa@fareportal.com>
2024-04-09 14:15:01 +0530

    introduction to next js

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.main}>
+        <h1>About Page Using File Based Routing</h1>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+const Login = () => {
+  return (
+    <div className={styles.main}>
+        <h1>Login Page Using File Based Routing</h1>
+    </div>
+  )
+}
+
+export default Login;
